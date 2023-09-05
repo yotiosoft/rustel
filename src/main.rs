0ae8a8789d8063c7eedef21064d1c0459e3d9ce5
YotioSoft <ytani0323@gmail.com>
2023-09-05 16:14:27 +0900

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use std::{net::{ToSocketAddrs, TcpStream}, io::BufRead, io::BufReader, io::Write, io::{BufWriter, Read}};
+use std::{net::{ToSocketAddrs, TcpStream}, io::BufReader, io::Write, io::{BufWriter, Read}};
+use encoding_rs;
 
-fn read_something(mut stream: &TcpStream) -> Result<(), std::io::Error> {
-    let mut buf_reader = BufReader::new(&mut stream);
-    let mut buffer: [u8; 1024] = [0; 1024];
+fn telnet_read(stream: &TcpStream) -> Result<String, std::io::Error> {
+    let mut buf_reader = BufReader::new(stream);
+    let mut buffer: [u8; 2] = [0; 2];
     buf_reader.read(&mut buffer)?;
-    println!("Received message: {:?}", buffer);
-    Ok(())
-}
-
-fn write_something(writer: &mut BufWriter<&TcpStream>, message: &str) -> Result<(), std::io::Error> {
-    let msg = String::from(message);
-    println!("Sending message: {}", msg);
-    writer.write(msg.as_bytes())?;
-    writer.flush()?;
-    Ok(())
-}
-
-fn write_u64(writer: &mut BufWriter<&TcpStream>, message: u64) -> Result<(), std::io::Error> {
-    println!("Sending message: {}", message);
-    writer.write(message.to_string().as_bytes())?;
-    writer.flush()?;
-    Ok(())
+    //println!("Received message: {:?}", buffer);
+    let (cow, _, _) = encoding_rs::SHIFT_JIS.decode(&buffer);
+    let text = cow.into_owned();
+    std::io::stdout().flush()?;
+    Ok(text)
 }
 
 fn main() {
@@ -37,12 +26,35 @@ fn main() {
         match TcpStream::connect(address) {
             Ok(stream) => {
                 println!("Connected to the server!");
-                let str = read_something(&stream).unwrap();
-                //write_u64(&mut writer, 0xFFFC24).unwrap();
+                loop {
+                    let str = telnet_read(&stream).unwrap();
+                    print!("{}", str);
+                    std::io::stdout().flush().unwrap();
+                    if str == "\0" {
+                        break;
+                    }
+                }
             },
             Err(e) => println!("Failed to connect: {}", e),
         }
     } else {
         println!("No IPv4 address found");
     }
+}
+
+
+
+fn write_something(writer: &mut BufWriter<&TcpStream>, message: &str) -> Result<(), std::io::Error> {
+    let msg = String::from(message);
+    println!("Sending message: {}", msg);
+    writer.write(msg.as_bytes())?;
+    writer.flush()?;
+    Ok(())
+}
+
+fn write_u64(writer: &mut BufWriter<&TcpStream>, message: u64) -> Result<(), std::io::Error> {
+    println!("Sending message: {}", message);
+    writer.write(message.to_string().as_bytes())?;
+    writer.flush()?;
+    Ok(())
 }
