ea40916ce958142b039ba95debda403d989cbcd0
YotioSoft <ytani0323@gmail.com>
2023-09-07 18:46:04 +0900

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::{ToSocketAddrs, TcpStream};
-use std::io::{BufReader, Write, Read};
+use std::io::{Write, BufWriter};
+
+use tokio::net::{ToSocketAddrs, TcpStream};
+use tokio::io::{BufReader, AsyncWrite, AsyncRead, AsyncReadExt};
 use encoding_rs;
 
 const TCP_LEN_MAX: usize = 65536;
@@ -14,10 +16,10 @@ enum IPv {
     IPv6,
 }
 
-fn telnet_read_utf8(stream: &TcpStream) -> Result<Option<String>, std::io::Error> {
+async fn telnet_read_utf8(stream: &mut TcpStream) -> Result<Option<String>, std::io::Error> {
     let mut buf_reader = BufReader::new(stream);
     let mut buffer = String::new();
-    buf_reader.read_to_string(&mut buffer)?;
+    buf_reader.read_to_string(&mut buffer);
     //println!("Received message: {:?} {}", buffer, text);
     if buffer.len() == 0 {
         return Ok(None);
@@ -25,10 +27,10 @@ fn telnet_read_utf8(stream: &TcpStream) -> Result<Option<String>, std::io::Error
     Ok(Some(buffer))
 }
 
-fn telnet_read_sjis(stream: &TcpStream) -> Result<Option<String>, std::io::Error> {
+async fn telnet_read_sjis(stream: &mut TcpStream) -> Result<Option<String>, std::io::Error> {
     let mut buf_reader = BufReader::new(stream);
     let mut buffer: [u8; TCP_LEN_MAX] = [0; TCP_LEN_MAX];
-    buf_reader.read(&mut buffer)?;
+    buf_reader.read(&mut buffer);
     if buffer[0] == 0 {
         return Ok(None);
     }
@@ -38,21 +40,21 @@ fn telnet_read_sjis(stream: &TcpStream) -> Result<Option<String>, std::io::Error
     Ok(Some(text))
 }
 
-fn telnet_read(stream: &TcpStream, encode: &Encode) -> Result<Option<String>, std::io::Error> {
+async fn telnet_read(stream: &mut TcpStream, encode: &Encode) -> Result<Option<String>, std::io::Error> {
     match encode {
-        Encode::UTF8 => telnet_read_utf8(stream),
-        Encode::SHIFT_JIS => telnet_read_sjis(stream),
+        Encode::UTF8 => telnet_read_utf8(stream).await,
+        Encode::SHIFT_JIS => telnet_read_sjis(stream).await,
     }
 }
 
-fn telnet_write_utf8(stream: &TcpStream, str: &str) -> Result<(), std::io::Error> {
-    let mut buf_writer = stream;
+async fn telnet_write_utf8(stream: &mut TcpStream, str: &str) -> Result<(), std::io::Error> {
+    let mut buf_writer = BufWriter::new(stream);
     buf_writer.write(str.as_bytes())?;
     buf_writer.flush()?;
     Ok(())
 }
 
-fn telnet_write_sjis(stream: &TcpStream, str: &str) -> Result<(), std::io::Error> {
+async fn telnet_write_sjis(stream: &TcpStream, str: &str) -> Result<(), std::io::Error> {
     let mut buf_writer = stream;
     let (cow, _, _) = encoding_rs::SHIFT_JIS.encode(str);
     buf_writer.write(&cow)?;
@@ -67,7 +69,22 @@ fn telnet_write(stream: &TcpStream, encode: &Encode, str: &str) -> Result<(), st
     }
 }
 
-fn main() {
+async fn telnet_input(stream: &TcpStream, encode: &Encode) -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+    if let Ok(_) = std::io::stdin().read_line(&mut input) {
+        if input.len() == 0 {
+            return Ok(());
+        }
+        telnet_write(stream, encode, &input)?;
+    }
+    else {
+        return Ok(());
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
     let host = "koukoku.shadan.open.ad.jp";
     //let host = "india.colorado.edu";
     let port = 23;
@@ -84,7 +101,7 @@ fn main() {
     if let Some(address) = address {
         println!("Found an IPv4 address: {}", address);
 
-        match TcpStream::connect(address) {
+        match TcpStream::connect(address).await {
             Ok(stream) => {
                 println!("Connected to the server!");
                 loop {
@@ -106,16 +123,7 @@ fn main() {
                     }
 
                     // write
-                    let mut input = String::new();
-                    if let Ok(_) = std::io::stdin().read_line(&mut input) {
-                        if input.len() == 0 {
-                            break;
-                        }
-                        telnet_write(&stream, &encode, &input).unwrap();
-                    }
-                    else {
-                        break;
-                    }
+                    telnet_input(&stream, &encode).await.unwrap()
                 }
             },
             Err(e) => println!("Failed to connect: {}", e),
